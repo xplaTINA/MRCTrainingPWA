ada39593c21a07e29acb61de4bbf11b7387ab5e9
Plat Raph <usuke411@gmail.com>
2024-10-01 18:50:03 +0900

    a

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ async function postMessage() {
       body: JSON.stringify({
         conversationId: username + "-" + receiverID,
         timestamp: new Date().toISOString(),
-        messageID: username + messageText,
+        messageID: generateSHA256Hash(username + new Date().toISOString()),
         messageText: messageText,
         receiverID: receiverID,
         senderID: username,
